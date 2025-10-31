fc83eede2c518734f2e318aae3d02dfbae2bfa8d
Niklas Goellmann <ngoellmann@gmail.com>
2025-10-31 17:10:39 +0100

    first changes for embedding

--- src/operators.py
+++ src/operators.py
@@ -68,6 +68,39 @@ class CoulombOperator:
             A vector of orbitals after applying the Coulomb operator.
         """
         return np.array([(self.potential * phi).crop(self.precision / 10) for phi in self.Phi])
+    
+class CoulombInteractionOperator:
+    """The Coulomb interaction operator between two sets of orbitals.
+    
+    Implements the Coulomb operator for a vector of orbitals for use in the SCF procedure.
+    Outputs a vector of orbitals after applying the Coulomb operator to each input orbital.
+    
+    Attributes:
+        mra: The multi-resolution analysis (MRA) object.
+        Phi: The vector of orbitals.
+        precision: The desired precision for the operator.
+    """
+    def __init__(self, mra, Phi, precision):
+        """Initializes the Coulomb operator based on the input orbitals."""
+        self.mra = mra
+        self.Phi = Phi
+        self.precision = precision
+        # initialize the Poisson operator
+        poisson = vp.PoissonOperator(mra, precision)
+
+        # compute the total electron density rho
+        rho = self.Phi[0]**2
+        for i in range(1, len(self.Phi)):
+            rho += self.Phi[i]**2
+        # apply the Poisson operator to rho to get the Coulomb potential
+        self.potential = 4.0 * np.pi * poisson(rho).crop(self.precision / 10)
+
+    def __call__(self, Phi):
+        """Applies the Coulomb operator to each orbital in the input vector.
+        Returns:
+            A vector of orbitals after applying the Coulomb operator.
+        """
+        return np.array([(self.potential * phi).crop(self.precision / 10) for phi in Phi])
 
 class ExchangeOperator:
     """The Exchange operator.
@@ -99,6 +132,39 @@ class ExchangeOperator:
             tmp = (self.Phi[0] * self.poisson(phi * self.Phi[0])).crop(self.precision / 10)
             for i in range (1, len(self.Phi)):
                 tmp += (self.Phi[i] * self.poisson(phi * self.Phi[i])).crop(self.precision / 10)
+            out.append(tmp)
+        return 4.0 * np.pi * np.array([phi.crop(self.precision / 10) for phi in out])
+    
+class ExchangeInteractionOperator:
+    """The Exchange interaction operator between two sets of orbitals.
+    
+    Implements the Exchange operator for a vector of orbitals for use in the SCF procedure.
+    Outputs a vector of orbitals after applying the Exchange operator to each input orbital.
+    
+    Attributes:
+        mra: The multi-resolution analysis (MRA) object.
+        Psi: The vector of orbitals.
+        precision: The desired precision for the operator.
+    """
+    def __init__(self, mra, Psi, precision):
+        """Initializes the Exchange operator based on the input orbitals."""
+        self.mra = mra
+        self.Psi = Psi
+        self.precision = precision
+        # Initialize the Poisson operator
+        self.poisson = vp.PoissonOperator(mra, precision)
+
+    def __call__(self, Phi):
+        """Applies the Exchange operator to each orbital in the input vector.
+        Returns:
+            A vector of orbitals after applying the Exchange operator.
+        """
+        out = []
+        # for the exchange, two loops over orbitals are needed
+        for psi in self.Psi:
+            tmp = (Phi[0] * self.poisson(psi * Phi[0])).crop(self.precision / 10)
+            for i in range (1, len(Phi)):
+                tmp += (Phi[i] * self.poisson(psi * Phi[i])).crop(self.precision / 10)
             out.append(tmp)
         return 4.0 * np.pi * np.array([phi.crop(self.precision / 10) for phi in out])
 
